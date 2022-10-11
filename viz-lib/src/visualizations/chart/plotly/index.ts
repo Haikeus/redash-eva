146108b7b62e99c02233d0f136375f39e5881001
Haikeus <haikeus.doom@gmail.com>
2022-10-11 15:17:12 +0300

    Import and register indicator

--- viz-lib/src/visualizations/chart/plotly/index.ts
+++ viz-lib/src/visualizations/chart/plotly/index.ts
@@ -9,6 +9,11 @@ import histogram from "plotly.js/lib/histogram";
 import box from "plotly.js/lib/box";
 // @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'plot... Remove this comment to see the full error message
 import heatmap from "plotly.js/lib/heatmap";
+/* eslint-disable */
+// @ts-ignore
+import indicator from "plotly.js/lib/indicator";
+/* eslint-enable */
+
 
 import prepareData from "./prepareData";
 import prepareLayout from "./prepareLayout";
@@ -18,7 +23,7 @@ import updateChartSize from "./updateChartSize";
 import { prepareCustomChartData, createCustomChartRenderer } from "./customChartUtils";
 
 // @ts-expect-error ts-migrate(2339) FIXME: Property 'register' does not exist on type 'typeof... Remove this comment to see the full error message
-Plotly.register([bar, pie, histogram, box, heatmap]);
+Plotly.register([bar, pie, histogram, box, heatmap, indicator]);
 // @ts-expect-error ts-migrate(2339) FIXME: Property 'setPlotConfig' does not exist on type 't... Remove this comment to see the full error message
 Plotly.setPlotConfig({
   modeBarButtonsToRemove: ["sendDataToCloud"],
